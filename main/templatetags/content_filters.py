561747a6c8868b04ecca3f9cbcced928cd14385c
Bella5768 <bell@csig.edu.gn>
2026-07-06 12:04:09 +0000

    Fixer erreur TemplateSyntaxError filter range dans testimonials

--- main/templatetags/content_filters.py
+++ main/templatetags/content_filters.py
@@ -16,3 +16,21 @@ def clean_html_excerpt(value):
     text = re.sub(r'<[^>]*>', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
     return text
+
+
+@register.filter
+def range_filter(value):
+    """Create a range from 0 to value-1"""
+    try:
+        return range(int(value))
+    except (ValueError, TypeError):
+        return range(0)
+
+
+@register.filter
+def range_from(value, start):
+    """Create a range from start to value-1"""
+    try:
+        return range(int(start), int(value))
+    except (ValueError, TypeError):
+        return range(0)
